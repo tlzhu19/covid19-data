492c0a977fe9418c209ad6fe8e2ff115ec573548
tlzhu19 <tiffanyzhu19@gmail.com>
2020-10-11 13:58:23 -0400

    Update data on 2020-10-11

--- extract_data.py
+++ extract_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from urllib.request import urlopen
+import subprocess as cmd
+from datetime import datetime
 
 
 # Filter out a few states
@@ -10,3 +12,12 @@ df_filtered = counties_df[counties_df['state'].isin(STATES)]
 
 df_filtered.to_csv('us-counties-filtered.csv', index=False)
 
+
+# Push to Git
+try:
+    cmd.run("git add .", check=True, shell=True)
+    update_message = '"Update data on {}"'.format(datetime.today().strftime('%Y-%m-%d'))
+    cmd.run('git commit -m {}'.format(update_message), check=True, shell=True)
+    cmd.run("git push -u origin master -f", check=True, shell=True)
+except:
+    print("Error git automation")
