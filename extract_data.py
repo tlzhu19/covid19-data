e3c19494baf83ea92154e243cbdfc371dd278cf7
tlzhu19 <tiffanyzhu19@gmail.com>
2020-10-11 16:21:22 -0400

    Revert "Update data on 2020-10-11 15:54:06"
    This reverts commit 4effc96ec683af26081678edb64b6b78f059c9df.

--- extract_data.py
+++ extract_data.py
@@ -11,7 +11,6 @@ counties_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/u
 counties_df = pd.read_csv(counties_url, dtype=str)
 df_filtered = counties_df[counties_df['state'].isin(STATES)]
 
-# Save filtered file
 df_filtered.to_csv('us-counties-filtered.csv', index=False)
 
 
@@ -21,7 +20,7 @@ try:
     cmd.run("/usr/local/bin/git -C {} add .".format(github_dir), check=True, shell=True)
     update_message = '"Update data on {}"'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     cmd.run('/usr/local/bin/git -C {} commit -m {}'.format(github_dir, update_message), check=True, shell=True)
-    cmd.run("/usr/local/bin/git -C {} push -u origin master".format(github_dir), check=True, shell=True)
+    cmd.run("/usr/local/bin/git -C {} push -u origin master -f".format(github_dir), check=True, shell=True)
 except Exception as e:
     print("Error with extract_data.py")
     print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
