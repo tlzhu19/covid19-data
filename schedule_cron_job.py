f205b9d92b598f184972a3bc5c71f74bd9590b5b
tlzhu19 <tiffanyzhu19@gmail.com>
2020-10-12 10:30:05 -0400

    Update data on 2020-10-12 10:30:05

--- schedule_cron_job.py
+++ schedule_cron_job.py
@@ -1,6 +1,5 @@
 from crontab import CronTab
 import os 
-# from datetime import datetime
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 python_script = os.path.join(dir_path, 'extract_data.py')
@@ -11,6 +10,7 @@ In terminal:
 * To check jobs: crontab -l
 * To delete all jobs: crontab -r
 * To check errors: vim /var/mail/tiffanyzhu
+* Delete mail: cat /dev/null >/var/mail/tiffanyzhu
 """
 
 cron = CronTab(user='tiffanyzhu')
@@ -20,19 +20,11 @@ job_command = '{} {}'.format(python_environment, python_script)
 job = cron.new(command=job_command, comment='coviddata')
 
 # Run job every 1 minute (to test)
-job.minute.every(1)
+# job.minute.every(1)
 
-# Run job 8am every day
-# job.minute().on(0)
-# job.hour().on(8)
+# Run job 10:30am every day
+job.minute.on(30)
+job.hour.on(10)
 print(job)
-
-
-
-# # Git pushing command
-# job_command_git = 'cd /Users/tiffanyzhu/projects/covid19-projects/covid19-data/ && git add . && git commit -m {} && git push -u origin master -f'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-# job_git = cron.new(command=job_command_git, comment='coviddatagit')
-# job_git.minute.every(1)
-# print(job_git)
 
 cron.write()
